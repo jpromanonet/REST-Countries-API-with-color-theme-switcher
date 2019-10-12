4e1e9aacc4e197d872653f683289a698fe23a384
sinamoraddar <sinamoraddar@gmail.com>
2019-10-12 13:21:52 +0330

    Updates

---
+++ precache-manifest.356ee681ee1964f961c37b0eac4aef82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56810a638fb847cafe74daa661fbdb2",
+    "url": "/REST-Countries-API-with-color-theme-switcher/index.html"
+  },
+  {
+    "revision": "9babc8dacd8e9f478f0a",
+    "url": "/REST-Countries-API-with-color-theme-switcher/static/css/main.59b78202.chunk.css"
+  },
+  {
+    "revision": "124c8485e31c0206608f",
+    "url": "/REST-Countries-API-with-color-theme-switcher/static/js/2.12ee4bcb.chunk.js"
+  },
+  {
+    "revision": "9babc8dacd8e9f478f0a",
+    "url": "/REST-Countries-API-with-color-theme-switcher/static/js/main.56c44fb4.chunk.js"
+  },
+  {
+    "revision": "2e2d4472e08cf2b45172",
+    "url": "/REST-Countries-API-with-color-theme-switcher/static/js/runtime~main.5947ff85.js"
+  }
+]);
