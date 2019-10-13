d6a92cba6a180f996ad3231e118bddd6c37dba53
sinamoraddar <sinamoraddar@gmail.com>
2019-10-13 23:20:38 +0330

    Updates

--- precache-manifest.e9a56e2b339f5f176c0d89c38df6fecb.js
+++ precache-manifest.e9a56e2b339f5f176c0d89c38df6fecb.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "29ae5e88d3be9f222f00b3785c99e431",
+    "revision": "0678612344041bf42d308a3087ee458f",
     "url": "/REST-Countries-API-with-color-theme-switcher/index.html"
   },
   {
-    "revision": "48cd8fcdb945e4258251",
+    "revision": "67e6a0dbdcff56d0d4e7",
     "url": "/REST-Countries-API-with-color-theme-switcher/static/css/main.fa348dbd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/REST-Countries-API-with-color-theme-switcher/static/js/2.12ee4bcb.chunk.js"
   },
   {
-    "revision": "48cd8fcdb945e4258251",
-    "url": "/REST-Countries-API-with-color-theme-switcher/static/js/main.e0b7a3ce.chunk.js"
+    "revision": "67e6a0dbdcff56d0d4e7",
+    "url": "/REST-Countries-API-with-color-theme-switcher/static/js/main.2f521cde.chunk.js"
   },
   {
     "revision": "2e2d4472e08cf2b45172",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REST-Countries-API-with-color-theme-switcher/precache-manifest.3d80628bf8bd0dd33b5897177db3fba3.js"
+  "/REST-Countries-API-with-color-theme-switcher/precache-manifest.e9a56e2b339f5f176c0d89c38df6fecb.js"
 );
 
 self.addEventListener('message', (event) => {
